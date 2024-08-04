5871bcc264f07630aad71ae61866b394849cebe7
Carlos Uziel Perez Malla <carlosuzielpm@gmail.com>
2024-08-04 10:40:41 +0100

    fix(env): fix environment dependencies

--- setup_env.sh
+++ setup_env.sh
@@ -4,10 +4,10 @@ mamba create -n bioinfo -c nvidia -c rapidsai -c conda-forge -c bioconda -y \
 rpy2 typer pydantic pandas umap-learn plotly python-kaleido matplotlib-base \
 jupyterlab ipywidgets tqdm autoflake black black-jupyter pylint flake8 \
 bioconductor-tcgabiolinks bioconductor-deseq2 bioconductor-complexheatmap \
+bioconductor-sesame bioconductor-sesamedata \
 bioconductor-apeglm bioconductor-enhancedvolcano r-pheatmap r-venndiagram \
 r-rcolorbrewer bioconductor-vsn bioconductor-annotationdbi r-devtools \
-r-ashr r-gprofiler2 r-irkernel bioconductor-pathview bioconductor-sbgnview \
-bioconductor-sesame bioconductor-sesamedata bioconductor-methylkit \
+r-ashr r-gprofiler2 r-irkernel bioconductor-pathview bioconductor-methylkit \
 bioconductor-minfi bioconductor-limma r-readxl r-msigdbr r-ggridges \
 bioconductor-dmrcate bioconductor-dmrcatedata r-ggnewscale trim-galore \
 bioconductor-annotatr bioconductor-minfidata r-scatterplot3d r-psych \
@@ -16,7 +16,7 @@ bioconductor-txdb.hsapiens.ucsc.hg38.knowngene bioconductor-do.db \
 bioconductor-clusterprofiler bioconductor-enrichplot r-ggupset \
 bioconductor-dose bioconductor-fgsea r-europepmc bioconductor-reactomepa \
 fastqc cutadapt star htseq multiqc samtools bamtools r-biocmanager \
-seaborn=0.12.2 fastcluster r-wgcna r-paralleldist bioconductor-biocparallel \
+seaborn=0.12.2 fastcluster r-paralleldist bioconductor-biocparallel \
 r-languageserver radian r-httpgd bioconductor-ggtree kneed openpyxl \
 shap  xgboost tblib r-jsonlite fish rich rich-cli natsort bioconductor-sva \
 r-tidyverse r-purrr matplotlib-venn wordcloud biopython fonttools \
@@ -26,7 +26,8 @@ bioconductor-cola bioconductor-simplifyenrichment sra-tools \
 scikit-learn networkx nltk r-magick pygraphviz bismark pigz \
 bioconductor-bsseq bioconductor-dss bioconductor-biseq deeptools \
 bioconductor-txdb.mmusculus.ucsc.mm10.knowngene bioconductor-chipseeker \
-bioconductor-rgreat bioconductor-org.mm.eg.db r-r.filesets python-docx
+bioconductor-rgreat bioconductor-org.mm.eg.db r-r.filesets python-docx \
+pyarrow
 
 conda activate bioinfo
 
@@ -44,4 +45,9 @@ Rscript -e 'library(devtools); install_github("al2na/methylKit", build_vignettes
 Rscript -e 'old.packages(repos="https://cloud.r-project.org/"); update.packages(ask=FALSE, repos="https://cloud.r-project.org/")'
 Rscript -e 'remove.packages("DO.db"); BiocManager::install("DO.db"); BiocManager::install(ask=FALSE)'
 Rscript -e 'BiocManager::install("BioinformaticsFMRP/TCGAbiolinksGUI.data"); BiocManager::install("BioinformaticsFMRP/TCGAbiolinks")'
-Rscript -e 'IRkernel::installspec()'
+Rscript -e 'BiocManager::install()' # to update all bioconductor packages
+Rscript -e 'IRkernel::installspec()' # to install R kernel for Jupyter
+
+# package fixes as of 04/08/2024
+Rscript -e 'devtools::install_github('https://github.com/Bioconductor/BiocFileCache.git')'
+Rscript -e 'remotes::install_version("matrixStats", version="1.1.0")'
