9f2ae8529ed4bc41cf41599ec348f74ad6f5e899
Carlos Uziel Perez Malla <carlosuzielpm@gmail.com>
2024-08-05 22:20:44 +0100

    feat(forking): update function so that rpy2 objets run in forked multi-threading

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,32 @@
+import contextvars
 import logging
+import traceback
+from contextlib import contextmanager
 from typing import Callable, Dict
 
-from rich.console import Console
+import rpy2.robjects as ro
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
 
-console = Console()
+logger = logging.getLogger(__name__)
+
+
+@contextmanager
+def context_wrapper():
+    ctx = contextvars.copy_context()
+    yield lambda func, *args, **kwargs: ctx.run(func, *args, **kwargs)
 
 
 def run_func_dict(kwargs: Dict, func: Callable):
+    logger.info(f"Starting execution of {func.__name__} with arguments: {kwargs}")
     try:
-        return func(**kwargs)
+        with localconverter(ro.default_converter + pandas2ri.converter):
+            with context_wrapper() as run_in_context:
+                result = run_in_context(func, **kwargs)
+                logger.info(
+                    f"Successfully executed {func.__name__} with result: {result}"
+                )
+                return result
     except Exception as e:
-        logging.error(e)
-        console.print_exception(max_frames=20)
-        return None
+        logger.error(f"Error occurred while executing {func.__name__}: {e}")
+        logger.debug(traceback.print_exc())
