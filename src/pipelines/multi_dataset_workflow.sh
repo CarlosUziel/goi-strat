e586d13d40629c61ec8683dd7073c6c99fb44c95
Carlos Uziel Perez Malla <carlosuzielpm@gmail.com>
2024-08-06 19:44:21 +0100

    feat(multi_dataset): add multi_dataset workflow script

---
+++ src/pipelines/multi_dataset_workflow.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# run from ~/biopipes/src/pipelines
+
+## 1. Download, process and split data
+python data/run/rna_seq/multi_dataset/download_data.py --threads 8 && \
+python data/run/rna_seq/multi_dataset/generate_gsva.py --threads 8 && \
+python data/run/rna_seq/multi_dataset/goi_perc_splits_annotation.py && \
+python data/run/rna_seq/multi_dataset/goi_gsva_splits_annotation.py
+
+## 2. Differential analyses
+python differential_expression/run/multi_dataset/goi_gsva.py --threads 16  && \
+python differential_expression/run/multi_dataset/goi_perc.py --threads 16 && \
+python differential_enrichment/run/multi_dataset/goi_gsva.py --threads 16 && \
+python differential_enrichment/run/multi_dataset/goi_perc.py --threads 16
